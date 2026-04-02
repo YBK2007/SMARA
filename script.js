d554cc7c2507a3db5cf7a52abbf2977e4eaf27d8
YBK2007 <yuyusbk@gmail.com>
2026-04-02 22:29:41 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// GSAP animation
-gsap.from(".hero h1", {opacity:0, y:-50, duration:1});
-gsap.from(".hero p", {opacity:0, y:50, duration:1, delay:0.3});
+gsap.from("h1", {opacity:0, y:-50, duration:1});
+gsap.from("p", {opacity:0, y:50, duration:1, delay:0.3});
 
-// WhatsApp CTA
 function openWhatsApp() {
   const number = "6281957147033";
-  const text = encodeURIComponent("Hello, I'm interested in SMARA Boutique Residence Cilandak");
+  const text = encodeURIComponent("Hello, I'm interested in Smara Boutique Residence Cilandak");
   window.open(`https://wa.me/${number}?text=${text}`, "_blank");
 }
