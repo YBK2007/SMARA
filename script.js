9b89e98ab725af41f35237b55bdb45977b406424
YBK2007 <yuyusbk@gmail.com>
2026-04-03 19:16:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+gsap.from("h1", {opacity:0, y:-50, duration:1});
+
 function openWhatsApp() {
-    const number = "6281957147033";
-    const text = encodeURIComponent("Hello, I'm interested in SMARA Residence");
-    window.open(`https://wa.me/${number}?text=${text}`, "_blank");
-  }
+  const number = "6281957147033";
+  const text = encodeURIComponent("Hello, saya tertarik dengan SMARA Boutique Residence");
+  window.open(`https://wa.me/${number}?text=${text}`, "_blank");
+}
